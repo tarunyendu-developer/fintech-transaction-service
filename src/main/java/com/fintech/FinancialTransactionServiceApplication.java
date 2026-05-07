e57af2b2a1fa4c1e1f004e8504f34946981711a7
tarunyendu-developer <tarunyendu2807@gmail.com>
2026-05-07 05:43:55 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/fintech/FinancialTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintech/FinancialTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
